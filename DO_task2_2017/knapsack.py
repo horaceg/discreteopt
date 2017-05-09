7df8ee19cb73453f6420153ac136813fb4b6319d
Horace Guy <horace.guy.k@gmail.com>
2017-05-10 01:25:29 +0200

    Changed the structure of sovler module

--- DO_task2_2017/knapsack.py
+++ DO_task2_2017/knapsack.py
@@ -1,5 +1,10 @@
 
 import numpy as np
+from solver import Solver
+import sys
+
+sys.setrecursionlimit(2500)
+
 
 def read_numbers(data_file):
     input_data_file = open(data_file, 'r')
@@ -7,12 +12,13 @@ def read_numbers(data_file):
     input_data_file.close()
 
     numbers = np.array([])
-    for i_line in xrange(len(input_data)):
+    for i_line in range(len(input_data)):
         entries = input_data[i_line].split()
         entries = filter(None, entries) # remove empty entries
         line_numbers = [ float(x) if x.lower != "inf" else float("inf") for x in entries ]
         numbers = np.append(numbers, line_numbers)
     return numbers
+
 
 def read_data(data_file):
     numbers = read_numbers(data_file)
@@ -29,7 +35,7 @@ def read_data(data_file):
     # get data on the items
     value = np.zeros(num_items, dtype = 'float')
     size = np.zeros(num_items, dtype = 'float')
-    for i_item in xrange(num_items):
+    for i_item in range(num_items):
         value[i_item] = float(numbers[cur_entry])
         cur_entry += 1
         size[i_item] = float(numbers[cur_entry])
@@ -37,22 +43,31 @@ def read_data(data_file):
         
     return value, size, capacity
 
+
 def make_dummy_solution(value, size, capacity):
     num_items = len(value)
     solution_value = 0
     solution_items = np.zeros(num_items, 'int')
     return solution_value, solution_items
 
-import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        file_location = sys.argv[1].strip()
-        value, size, capacity = read_data(file_location)
-
-        solution_value, solution_items = make_dummy_solution(value, size, capacity)
-        
-        print solution_value
-        print ' '.join(map(str, solution_items))
+        path = sys.argv[1].strip()
     else:
-        print 'This script requires an input file as command line argument.'
+        k = input("test sample number : ")
+        path = "task2_test{}.txt".format(k)
+
+    value, size, capacity = read_data(path)
+    print("Nb items :", len(size), "\nCapacity :", capacity, "\nvalue :\n", list(value), "\nsize :\n", list(size))
+
+    knapsack = Solver(value=value, size=size, capacity=capacity)
+    solution_value, solution_items = knapsack.solve_it()
+    #val_greedy, greedy_items = knapsack.greedy(value, size, capacity)
+    print("solution items")
+    print(' '.join(map(str, solution_items)))
+
+    outpath = path.split('.')[0] + "_solution.txt"
+    with open(outpath, 'w') as outfile:
+        outfile.write(str(solution_value)+'\n')
+        outfile.write(" ".join(map(str, solution_items)))
---
+++ DO_task2_2017/solver.py
@@ -0,0 +1,165 @@
+from functools import lru_cache
+import numpy as np
+from tco import *  # tail-call optimization
+from collections import deque
+
+
+class Solver:
+    
+    def __init__(self, value=[], size=[], capacity=0):
+        self.value = value
+        self.size = size
+        self.capacity = capacity
+        self.mask, self.inv_perm, self.ovalue, self.osize = self.dense_sort()
+
+    def solve_it(self):
+        if len(self.size) <= 200:
+            name, val, items = ("dynamic programming",) + self.dynamic()
+            items = np.array(items)
+        elif len(self.size) <= 200:
+            name, val, items = ("Branch and bound - depth-first",) + self.branch_bound()
+        else:
+            name, val, items = ("Branch and bound - limited discrepancy (1)",) + self.lds(errors=1)
+            # name, val, items = ("Branch and bound - iterative dfs",) + self.dfs_iter()
+        print("============ Solution found ===============", "Algorithm : {}".format(name), "value : {}".format(val),
+              "solution items sorted by density", sep='\n')
+        print(' '.join(map(str, items[self.mask])))
+
+        return val, items
+
+    def dense_sort(self):
+        # returns density-sorted value, size arrays
+        mask = (np.array(self.value) / np.array(self.size)).argsort()[::-1]
+        ovalue, osize = self.value[mask], self.size[mask]  # order by density
+        inv_perm = np.empty(len(self.size), dtype=np.int)
+        inv_perm[mask] = np.arange(len(self.size))  # to retrieve the original order
+        print("==== Density sort ====", "mask : ", list(mask), "ovalue : ", list(ovalue), "osize : ", list(osize), sep='\n')
+        return mask, inv_perm, ovalue, osize
+
+    def compute_val(self, dec_var):
+        # computes total value of a solution dec_var = [x1...xn] , xi in {0,1}
+        return np.dot(self.value.T, dec_var)
+
+    def dynamic(self):
+        @lru_cache(maxsize=None)  # memoization : stores computed values by function k()
+        def k(i, s):
+            if i == 0: return 0
+            elif self.size[i - 1] <= s: return max(k(i-1, s), self.value[i-1] + k(i-1, s - self.size[i-1]))
+            else: return k(i-1, s)
+
+        def findpath(i, s, val):
+            if i == 0: return []
+            elif val == k(i-1, s): return findpath(i-1, s, val) + [0]
+            else: return findpath(i-1, s - self.size[i-1], val - self.value[i-1]) + [1]
+
+        sol = k(len(self.size), self.capacity)
+        items = findpath(len(self.size), self.capacity, sol)
+        return sol, items
+
+    def heuristic(self, i, room):
+        # Estimates additional value to gain from i to end, having items sorted by density
+        # Is admissible, e.g. always >= to true value
+        v, s, item = 0, 0, i
+        while s < room and item < len(self.osize):
+            if self.osize[item] + s <= room:
+                s += self.osize[item]
+                v += self.ovalue[item]
+            else:
+                v += self.ovalue[item] * (room - s) / self.osize[item]
+                s = room
+            item += 1
+        return v
+
+    def greedy(self):
+        room, set, val = self.capacity, [], 0
+        for v, s in zip(self.ovalue, self.osize):
+            if s <= room:
+                set += [1]
+                room -= s
+                val += v
+            else:
+                set += [0]
+        return val, np.array(set)[self.inv_perm]
+
+    def lowbound(self):
+        print("======== Lower greedy bound ============")
+        val, items = self.greedy()
+        print("value : {}".format(val), "items by density", list(items[self.mask]), sep='\n')
+        return val, items[self.mask]
+
+    def branch_bound(self):
+        # depth-first search, having items sorted by density.
+        # produces stack overflow with test 5 because of recursion limits
+
+        def dfs(item, v, room, set):
+            nonlocal bestval, bestset
+            if item == len(self.osize):
+                if v > bestval:
+                    bestset = set
+                    bestval = v
+            else:
+                estim = v + self.heuristic(item, room)
+                if estim > bestval:
+                    if self.osize[item] <= room:
+                        dfs(item+1, v+self.ovalue[item], room-self.osize[item], set+[1])
+                    dfs(item+1, v, room, set+[0])
+
+        bestval, bestset = self.lowbound()
+        dfs(0, 0, self.capacity, [])
+        fset = np.array(bestset)[self.inv_perm]
+
+        assert self.compute_val(fset) == bestval
+        return bestval, fset
+
+    def dfs_iter(self):
+        root = {'item': 0, 'val': 0, 'room': self.capacity, 'set': []}
+        bestval, bestset = self.lowbound()
+        bestnode = {'item': None, 'val': bestval, 'room': None, 'set': bestset}
+        s = deque()
+        s.append(root)
+        while len(s) > 0:
+            node = s.pop()
+            if node['item'] == len(self.osize):
+                if node['val'] > bestnode['val']:
+                    bestnode = node
+            else:
+                estim = node['val'] + self.heuristic(node['item'], node['room'])
+                if estim > bestnode['val']:
+                    s.append({'item': node['item'] + 1, 'val': node['val'],
+                              'room': node['room'], 'set': node['set']+[0]})
+                    if self.osize[node['item']] <= node['room']:
+                        s.append({'item': node['item']+1, 'val': node['val']+self.ovalue[node['item']],
+                                  'room': node['room']-self.osize[node['item']], 'set': node['set']+[1]})
+        return bestnode['val'], np.array(bestnode['set'])[self.inv_perm]
+
+    def lds(self, errors=1):
+        # limited discrepancy search : greedy algorithm with up to k discrepancies
+
+        root = {'item': 0, 'val': 0, 'room': self.capacity, 'set': [], 'left':errors}
+        bestval, bestset = self.lowbound()
+        bestnode = {'item': None, 'val': bestval, 'room': None, 'set': bestset, 'left':0}
+        s = deque()  # stack
+        s.append(root)
+        left = errors
+        while len(s) > 0:
+            node = s.pop()
+            if node['item'] == len(self.osize):
+                if node['val'] > bestnode['val']:
+                    bestnode = node
+            else:
+                estim = node['val'] + self.heuristic(node['item'], node['room'])
+                if estim > bestnode['val']:
+                    if self.osize[node['item']] <= node['room']:
+                        if node['left'] >= 1:
+                            s.append({'item': node['item']+1, 'val': node['val'],
+                                      'room': node['room'], 'set': node['set']+[0],
+                                      'left': node['left']-1})
+                        s.append({'item': node['item']+1, 'val': node['val']+self.ovalue[node['item']],
+                                  'room': node['room']-self.osize[node['item']], 'set': node['set']+[1],
+                                  'left':node['left']})
+                    else:
+                        s.append({'item': node['item']+1, 'val': node['val'],
+                                  'room': node['room'], 'set': node['set']+[0],
+                                  'left': node['left']})
+
+        return bestnode['val'], np.array(bestnode['set'])[self.inv_perm]
